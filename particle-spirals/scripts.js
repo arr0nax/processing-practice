3335cdf86c8b138855a0b08777f7a01a2a0040d5
Clayton Collins <claytonccollins@gmail.com>
2017-09-23 12:40:17 -0700

    remove comments

--- particle-spirals/scripts.js
+++ particle-spirals/scripts.js
@@ -1,106 +1,45 @@
 var systems;
-var squareSize = 10;
+var squareSize = 50;
 var gridWidth;
 var gridHeight;
-var angle = 0;
-var centerX;
-var centerY;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   gridWidth = windowWidth/squareSize;
   gridHeight = windowHeight/squareSize;
-  centerX = windowWidth/2;
-  centerY = windowHeight/2;
   systems = [];
   system1 = new ParticleSystem();
   system1.addParticles();
   systems.push(system1);
   system2 = new ParticleSystem();
-  system2.addParticles();
-  for (var i=0; i<(gridWidth); i++) {
-    for (var j=0; j<(gridHeight); j++) {
-      system2.particles[i][j] = system1.particles[i][j];
-    }
-  }
+  system2.particles = system1.particles;
+  system2.angle = 1;
   systems.push(system2);
 }
 
 function draw() {
-  // background(255/2*sin(frameCount/90)+255/2,255/2*sin(frameCount/100)+255/2, 255/2*sin(frameCount/110)+255/2);
-  // background(255);
-  // systems[1].origin = [(mouseX-windowWidth/2), (mouseY-windowHeight/2)];
-//   switch(int(random(0,5))) {
-//     case 0:
-//       origin = [0.1,0];
-//     break;
-//     case 1:
-//       origin = [0,0.1];
-//         break;
-//
-//         case 1:
-//           origin = [0.1,0.1];
-//                 break;
-//             case 1:
-//               origin = [0,0];
-//                         break;
-//
-//     default:
-//     origin = [0,0];
-//
-// }
-  // systems[1].angle = (mouseX/windowWidth)*10;
-  for (var s=1; s<2; s++) {
+  for (var s=1; s<systems.length; s++) {
     for(var i=0; i<systems[s].particles.length; i++) {
       for(var j=0; j<systems[s].particles[i].length; j++) {
         if (systems[s].particles[i][j][0]) {
           push();
-          stroke(255/2*sin(frameCount/110)+255/2,255/2*sin(frameCount/120)+255/2, 255/2*sin(frameCount/130)+255/2);
-          fill(255/2*sin(frameCount/110)+255/2,255/2*sin(frameCount/120)+255/2, 255/2*sin(frameCount/130)+255/2);
+          stroke(255/2*sin(frameCount/1100)+255/2,255/2*sin(frameCount/1200)+255/2, 255/2*sin(frameCount/1300)+255/2);
+          fill(255/2*sin(frameCount/1100)+255/2,255/2*sin(frameCount/1200)+255/2, 255/2*sin(frameCount/1300)+255/2);
+          pointX = (i*squareSize);
+          pointY = (j*squareSize);
+          distance = systems[s].particles[i][j][1];
 
-          pointX = i*squareSize;
-          pointY = j*squareSize;
-          if (s === 1) {
-            systems[s].particles[i][j][1] += 0.005;
-          }
-          angle = systems[s].particles[i][j][1];
-
-          distance = Math.pow((Math.pow(pointX-centerX,2) + Math.pow(pointY-centerY,2)),0.5);
-
-          newX = centerX+(distance*Math.cos(angle));
-          // if (newX > windowWidth) {
-          //   while (newX > windowWidth) {
-          //     newX -= windowWidth;
-          //   }
-          // } else if (newX < 0) {
-          //   while (newX < 0) {
-          //     newX += windowWidth
-          //   }
-          // }
-          newY = centerY+(distance*Math.sin(angle));
-          // newY = newY+pointY+systems[s].origin[1];
-          // if (newY > windowHeight) {
-          //   while (newY > windowHeight) {
-          //     newY -= windowHeight;
-          //   }
-          // } else if (newY < 0) {
-          //   while (newY < 0) {
-          //     newY += windowHeight
-          //   }
-          // }
-          rect(newX,newY,squareSize,squareSize);
-          // rect(i*squareSize, j*squareSize, 10,10);
+          newX = (distance*Math.cos(systems[s].angle * Math.PI/180));
+          newX = pointX+newX+systems[s].origin[0];
+          newY = (distance*Math.sin(systems[s].angle * Math.PI/180));
+          newY = newY+pointY+systems[s].origin[1];
+          ellipse(newX,newY,squareSize,squareSize);
           pop();
-          // if(s===1){
-          //   stroke(255/2*sin(frameCount/120)+255/2,255/2*sin(frameCount/130)+255/2, 255/2*sin(frameCount/140)+255/2);
-          //   line(pointX,pointY,newX,newY);
-          // }
-        // } else {
-
         }
       }
     }
   }
+  systems[1].angle += 0.05;
 }
 
 var ParticleSystem = function () {
@@ -114,17 +53,10 @@ ParticleSystem.prototype.addParticles = function () {
     this.particles.push([]);
     for (var j=0; j<(gridHeight); j++) {
       distance = (Math.pow((Math.pow((i - this.origin[0]),2) + Math.pow((j - this.origin[1]),2)),0.5))*squareSize;
-
-      pixelX = i*squareSize;
-      pixelY = j*squareSize;
-      angle = atan((pixelX-centerX)/(pixelY-centerY));
-      console.log("point "+ i + ", "+j+ "is at "+angle+" degrees");
-
-
-      if(int(random(0,6)) === 0) {
-        this.particles[i].push([1,distance,angle]);
+      if(int(random(0,3)) === 0) {
+        this.particles[i].push([1,distance]);
       } else {
-        this.particles[i].push([0,distance,angle]);
+        this.particles[i].push([0,distance]);
       }
     }
   }
@@ -132,3 +64,7 @@ ParticleSystem.prototype.addParticles = function () {
 
 //*Math.sin(systems[s].angle * Math.PI/180)
 //*Math.cos(systems[s].angle * Math.PI/180)
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
